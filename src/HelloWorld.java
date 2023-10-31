a4db2370e381e14699673014cd768b6198cc0e6b
ksbleek <kyle.s.bleeker@gmail.com>
2023-10-31 12:11:00 -0500

    finnished syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,29 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
         int myFavoriteNumber = 5;
-
+//        String myString = 3.14159;
+//        System.out.println(myString);
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        class MyName = "Kyle";
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4 * x;
+//        int y = 2;
+//        x = 10 / y;
+//        y -= x;
     }
 
 }
