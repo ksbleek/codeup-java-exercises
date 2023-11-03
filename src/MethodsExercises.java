e6e4fb8b76d1b02eadc0acbe1468ef298bc22b0d
ksbleek <kyle.s.bleeker@gmail.com>
2023-11-03 16:08:35 -0500

    changed some stuff

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -14,7 +14,7 @@ public class MethodsExercises {
 //    public static int multiplication(int num1, int num2){
 //        return num1 * num2;
 //    }
-//    public static int division(int num1, int num2){
+//    public static double division(double num1, double num2){
 //        return num1 / num2;
 //    }
 //    public static int modulus(int num1, int num2){
