e9ab6383f1227bde0a5b1c7b23bbaa10e615b7a2
ksbleek <kyle.s.bleeker@gmail.com>
2023-11-08 16:20:39 -0600

    finished inheritance exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,40 @@
+package shapes;
+
+public class Rectangle {
+
+    protected int length;
+    protected int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+
+    public int getArea(){
+        return length * width;
+    };
+
+    public int getPerimeter(){
+        return 2 * (length + width);
+    };
+
+
+    //    getter and setters
+
+//    public int getLength() {
+//        return length;
+//    }
+//
+//    public void setLength(int length) {
+//        this.length = length;
+//    }
+//
+//    public int getWidth() {
+//        return width;
+//    }
+//
+//    public void setWidth(int width) {
+//        this.width = width;
+//    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,22 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+    private int side;
+
+    public Square(int side) {
+        super(side, side);
+        this.side = side;
+    }
+
+    @Override
+    public int getArea() {
+        return side * side;
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 4 * side;
+    }
+}
+
