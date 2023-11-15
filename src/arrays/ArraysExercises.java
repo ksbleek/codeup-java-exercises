a8eb65cb57d06b38ebd5288ff515b33cf95c6f3a
ksbleek <kyle.s.bleeker@gmail.com>
2023-11-15 14:03:05 -0600

    finished the arrays exercise

--- src/arrays/ArraysExercises.java
+++ src/arrays/ArraysExercises.java
@@ -8,5 +8,31 @@ public class ArraysExercises {
         System.out.println(numbers);
 
         System.out.println(Arrays.toString(numbers));
+        Person[] people = new Person[3];
+        people[0] = new Person("Jack");
+        people[1] = new Person("Sally");
+        people[2] = new Person("Ben");
+
+        printMyPeople(people);
+
+        Person [] morePeople = addPerson(people, new Person("Dave"));
+
+        printMyPeople(morePeople);
+    }
+
+    private static Person [] addPerson(Person[] ppl, Person person) {
+
+        Person [] morePpl = Arrays.copyOf(ppl, ppl.length + 1);
+        morePpl[morePpl.length - 1] = person;
+        return morePpl;
+
+    }
+
+    private static void printMyPeople(Person[] ppl) {
+
+        for (Person person : ppl) {
+            System.out.println(person);
+        }
+
     }
 }
---
+++ src/arrays/Person.java
@@ -0,0 +1,33 @@
+package arrays;
+
+public class Person {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void sayHello(){
+            System.out.printf("Hello, %s%n", name);
+        }
+
+        public Person() {
+        }
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
